8649b5bbfa6e039c5fbd319588f8c852a8e5bac5
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-11-13 11:18:58 +0530

    multiple-env

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -137,7 +137,7 @@ resource "aws_instance" "mysql" {
 
 resource "aws_iam_instance_profile" "mysql" {
   name = "mysql"
-  role = "EC2SSMParameterRead"
+  role = "EC2SSMParameterstore"
 }
 
 resource "terraform_data" "mysql" {
