09429c8004cdf7de5768795416ccbd3ad8ebeb45
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-11-15 13:46:31 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -12,11 +12,14 @@ REPO_URL=https://github.com/kukunuri-prasanna17/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
 
+
 mkdir -p $REPO_DIR
-mkdir -p /var/log/roboshop/
-touch  /var/log/roboshop/ansible.log
+sudo mkdir -p /var/log/roboshop/
+sudo touch /var/log/roboshop/ansible.log
+sudo chmod 755 /var/log/roboshop/ansible.log
 
 cd $REPO_DIR
+
 
 # check if ansible repo is already cloned or not
 
