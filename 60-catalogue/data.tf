a0d3f2e1e6c89e340257f2f18ebb0db244c50a13
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-11-17 14:48:47 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -28,9 +28,8 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 }
 
 data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project_name}/${var.environment}/vpc_id"
+  name = "/s${var.project_name}/${var.environment}/vpc_id"
 }
-
 
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
