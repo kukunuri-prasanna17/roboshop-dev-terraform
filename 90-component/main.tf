a7386801688456d96c0334e4faa5b26371224437
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-11-17 17:18:38 +0530

    terraform

---
+++ 90-component/main.tf
@@ -0,0 +1,6 @@
+module "component" {
+  source = "../terraform-roboshop-component"
+  component= var.component
+  rule_priority= var.rule_priority
+
+}
---
+++ 90-component/variables.tf
@@ -0,0 +1,15 @@
+variable "component" {
+  default = "catalogue"
+}
+
+variable "zone_id" {
+    default = "Z00127841VMI13X3SDGHZ"
+}
+
+variable "domain_name" {
+    default = "daws86s.cfd"
+}
+
+variable "rule_priority" {
+  default = "10"
+}
